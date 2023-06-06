2d04f6a6409cc01e73cf3a53806d9e289509aae3
kev0921 <kevin.hu0921@gmail.com>
2023-06-06 15:05:47 -0400

    added basic components for about and contact pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,39 @@
+import { VStack, Box, Flex, Spacer, HStack, Text, Heading, SimpleGrid, FormControl, FormLabel, Input, FormHelperText, Textarea } from "@chakra-ui/react";
+import { Form } from 'react-router-dom'
+
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <SimpleGrid ml="200px" mr="200px" mt="120px" columns={2}>
+      <VStack>
+        <Box width="100%">
+          <Heading>
+            Contact.
+          </Heading>
+          <Text mt="10px">
+            Get in touch with me via social media or email.
+          </Text>
+        </Box>
+
+        <Box mt="10" width="100%">
+          <Form ml="0" p="5px">
+            <FormControl>
+              <FormLabel> Name: </FormLabel>
+              <Input focusBorderColor='blue.600' type="text" name="name"></Input>
+            </FormControl>
+
+            <FormControl mt="10px">
+              <FormLabel> Email: </FormLabel>
+              <Input focusBorderColor='blue.600' type="email" name="email"></Input>
+            </FormControl>
+
+            <FormControl mt="10px">
+              <FormLabel> Message: </FormLabel>
+              <Textarea focusBorderColor='blue.600' name="message"></Textarea>
+            </FormControl>
+          </Form>
+        </Box>
+      </VStack>
+
+    </SimpleGrid>
   )
 }
