9730e605a3f366b40eb17f332a02d7914f84c359
kev0921 <kevin.hu0921@gmail.com>
2023-07-18 02:31:43 -0400

    optimizing images for build production

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ const Projects = () => {
 
                 <Card minHeight="550px" maxHeight="600px" borderRadius={20} boxShadow={"lg"}>
 
-                    <Image src="/public/img/MelodyMatch.png" alt="Image" minWidth="100%" maxWidth="100%" minHeight="40%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
+                    <Image src="/img/MelodyMatch.png" alt="Image" minWidth="100%" maxWidth="100%" minHeight="40%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
 
                     <CardBody fontWeight="normal" fontSize={[17, 20]} color="gray.600">
                         <Stack spacing='2.5'>
@@ -60,7 +60,7 @@ const Projects = () => {
 
                 <Card maxHeight="600px" borderRadius={20} boxShadow={"lg"}>
 
-                    <Image src="/public/img/PersonalWebsite.png" alt="Image" minWidth="100%" maxWidth="100%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
+                    <Image src="/dist/img/PersonalWebsite.png" alt="Image" minWidth="100%" maxWidth="100%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
 
                     <CardBody fontWeight="normal" fontSize={[17, 20]} color="gray.600">
                         <Stack spacing="2.5">
@@ -102,7 +102,7 @@ const Projects = () => {
 
                 <Card maxHeight="600px" borderRadius={20} boxShadow={"lg"}>
 
-                    <Image src="/public/img/MazeSolverRobot.png" alt="Image" minWidth="100%" maxWidth="100%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
+                    <Image src="dist/img/MazeSolverRobot.png" alt="Image" minWidth="100%" maxWidth="100%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
 
                     <CardBody fontWeight="normal" fontSize={[17, 20]} color="gray.600">
                         <Stack spacing="2.5">
@@ -133,7 +133,7 @@ const Projects = () => {
 
                 <Card maxHeight="600px" borderRadius={20} boxShadow={"lg"}>
 
-                    <Image src="/public/img/ClubWebsite.png" alt="Image" minWidth="100%" maxWidth="100%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
+                    <Image src="dist/img/ClubWebsite.png" alt="Image" minWidth="100%" maxWidth="100%" maxHeight="50%" objectFit="cover" borderTopRadius="xl" mx="auto"/>
 
                     <CardBody fontWeight="normal" fontSize={[17, 20]} color="gray.600">
                         <Stack spacing="2.5">
@@ -183,7 +183,7 @@ const Projects = () => {
             </SimpleGrid>
             
             <Heading mt="60px" mb="5px" size='lg'>My resume:</Heading>
-            <a href="public/resume.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="dist/resume.pdf" target="_blank" rel="noopener noreferrer">
                 <Button colorScheme="blue">View PDF</Button>
             </a>
 
