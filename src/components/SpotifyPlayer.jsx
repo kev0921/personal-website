042f4446bb52da3caa7a5a9ca6f485852e041210
kev0921 <kevin.hu0921@gmail.com>
2023-12-29 22:34:11 -0500

    spotify fix?

--- src/components/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import getNowPlayingItem from "./SpotifyApi";
-import "./SpotifyPlayer.css";
 
 function SpotifyPlayer(props) {
   const [result, setResult] = useState({});
@@ -17,13 +16,58 @@ function SpotifyPlayer(props) {
     });
   }, []);
 
+  const trackInfoStyle = {
+    width: "70%",
+    float: "left",
+    display: "block",
+  };
+
+  const trackTitleStyle = {
+    fontSize: "14px",
+    textAlign: "left",
+    textDecoration: "none",
+    verticalAlign: "middle",
+    overflow: "hidden",
+    whiteSpace: "nowrap",
+    textOverflow: "ellipsis",
+  };
+
+  const oddLinkStyle = {
+    color: "black",
+    fontWeight: "bold",
+    verticalAlign: "middle",
+    lineHeight: "15px",
+    letterSpacing: "0.2px",
+    padding: "10% 0 0 5%",
+  };
+
+  const evenLinkStyle = {
+    color: "gray",
+    fontSize: "12px",
+    letterSpacing: "0.1px",
+    padding: "5% 0 0 5%",
+  };
+
   return result.isPlaying ? (
     <div className="nowplayingcard">
       <div className="nowplayingcontainer-inner">
-        <img id="trackart" src={result.albumImageUrl}></img>
-        <div className="trackInfo">
-          <a id="tracktitle">{result.title}</a>
-          <a href="#" id="trackartist">
+        <img
+          id="trackart"
+          src={result.albumImageUrl}
+          style={{
+            maxWidth: "30%",
+            float: "left",
+            left: "0",
+            borderTopLeftRadius: "3px",
+            borderBottomLeftRadius: "3px",
+            // Add other image styles here...
+          }}
+        ></img>
+        <div className="trackInfo" style={trackInfoStyle}>
+          <a id="tracktitle" style={trackTitleStyle}>
+            {result.title}
+          </a>
+          <a href="#" id="trackartist" style={oddLinkStyle}>
             {result.artist}
           </a>
         </div>
